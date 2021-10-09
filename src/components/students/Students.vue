0fe07deb35c0b1a1feefeeabf7029cdb83d6ad29
unknown <hdnguyenn7702@gmail.com>
2021-10-09 13:26:03 +0700

    push content

--- src/components/students/Students.vue
+++ src/components/students/Students.vue
@@ -1,12 +1,25 @@
 <template>
     <div>
-        danh sách sinh viên
+     
     </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return {
+            listStudents:[], // dùng để chứa sinh viên sau khi gọi fetch thành công
+        }
+    }
+    mounted () {
+        // nó sẽ chạy ngay khi các node ( obecct html div gắn màn hình render)
+        // gọi API ( gọi tới sở dữ liệu)
+        fetch ("http://localhost:3000/ower")
+            .then (repos => repos.json())
+            .then (data => this.listStudents = data ) // đã lấy dữ liệu thành công.
+            
+        
+    }
 }
 </script>
 
