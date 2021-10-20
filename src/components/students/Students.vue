cfe096a28738e281d4fb332751c9f968d7ec0fb7
Nguyen pro <hdnguyen7702@gmail.com>
2021-10-21 00:26:27 +0700

    update data

--- src/components/students/Students.vue
+++ src/components/students/Students.vue
@@ -1,84 +1,120 @@
 <template>
-    <div id="students" class="container-fluid">
-      <div id="content" >
-        <header-add  />
-        <search-students :showStudents="showStudents" :tangshow="tangshow" :giamshow="giamshow" />
-      <div  id="table">
-            <div class="row no-gutters">  
-                <div class="col-1 border">Mã số SV
-                <span style="margin-left:10px"><awesome icon="sort" /></span>
-                </div>
-                <div class="col-1 border">Ảnh</div>
-                <div class="col-2 border">Họ tên
-                <span style="margin-left:10px"><awesome icon="sort" /></span>
-                </div>
-                <div class="col-1 border">Giới tính</div>
-                <div class="col-2 border">email</div>
-                <div class="col-2 border">nghành học</div>
-                <div class="col-1 border">Xem</div>
-                <div class="col-1 border">Sữa</div>
-                <div class="col-1 border">Xóa</div>
-            </div> 
-            <div class="row no-gutters border-top border-bottom row-custom" v-for="(student,index) in listRender" :key="index">
-                <div class="col-1 border-left border-right  ">{{student.maSV }}</div>
-                <div class="col-1 border-left border-right ">{{student.image }}</div>
-                <div class="col-2 border-left border-right ">{{student.fullName }}</div>
-                <div class="col-1 border-left border-right ">{{student.gender }}</div>
-                <div class="col-2 border-left border-right ">{{student.email }}</div>
-                <div class="col-2 border-left border-right ">{{student.nghanhHoc}}</div>
-                <div class="col-1 border-left border-right ">
-                  <!-- Xem chĩnh sữa xóa -->
-                  <router-link class="btn btn-info min-width-60px" :to="{ name: 'Student', params: { id: student.id } }" >Xem</router-link>
-                </div>
-                <div class="col-1 border-left border-right ">
-                  <router-link class="btn btn-primary min-width-60px"  :to="{ name: 'Edit', params: { id: student.id } }" >Sữa</router-link>
-                </div>
-                <div class="col-1 border-left border-right ">
-                  <button @click="confirm(student.id,student.fullName,student.maSV,students)" class="btn btn-danger min-width-60px">Xóa</button>
-                </div>
-            </div> 
+  <div id="students" class="container-fluid">
+    <div id="content">
+      <header-add />
+      <search-students
+        :showStudents="showStudents"
+        :tangshow="tangshow"
+        :giamshow="giamshow"
+      />
+      <div id="table">
+        <div class="row no-gutters">
+          <div class="col-1 border">
+            Mã số SV
+            <span style="margin-left: 10px"><awesome icon="sort" /></span>
+          </div>
+          <div class="col-1 border">Ảnh</div>
+          <div class="col-2 border">
+            Họ tên
+            <span style="margin-left: 10px"><awesome icon="sort" /></span>
+          </div>
+          <div class="col-1 border">Giới tính</div>
+          <div class="col-2 border">email</div>
+          <div class="col-2 border">nghành học</div>
+          <div class="col-1 border">Xem</div>
+          <div class="col-1 border">Sữa</div>
+          <div class="col-1 border">Xóa</div>
+        </div>
+        <div
+          class="row no-gutters border-top border-bottom row-custom"
+          v-for="(student, index) in listRender"
+          :key="index"
+        >
+          <div class="col-1 border-left border-right">{{ student.maSV }}</div>
+          <div class="col-1 border-left border-right">{{ student.image }}</div>
+          <div class="col-2 border-left border-right">
+            {{ student.fullName }}
+          </div>
+          <div class="col-1 border-left border-right">{{ student.gender }}</div>
+          <div class="col-2 border-left border-right">{{ student.email }}</div>
+          <div class="col-2 border-left border-right">
+            {{ student.nghanhHoc }}
+          </div>
+          <div class="col-1 border-left border-right">
+            <!-- Xem chĩnh sữa xóa -->
+            <router-link
+              class="btn btn-info min-width-60px"
+              :to="{ name: 'Student', params: { id: student.id } }"
+              >Xem</router-link
+            >
+          </div>
+          <div class="col-1 border-left border-right">
+            <router-link
+              class="btn btn-primary min-width-60px"
+              :to="{ name: 'Edit', params: { id: student.id } }"
+              >Sữa</router-link
+            >
+          </div>
+          <div class="col-1 border-left border-right">
+            <button
+              @click="
+                confirm(student.id, student.fullName, student.maSV, students)
+              "
+              class="btn btn-danger min-width-60px"
+            >
+              Xóa
+            </button>
+          </div>
+        </div>
       </div>
-                                
-  <!-- pagination -->
-  <div id="pagination">
-      <p>Show in 1 to {{ showStudents.length }} to {{ lengthArrStudents }}</p>
-      <div>
-        <span @click="previousStep">
-          <awesome icon="angle-double-left"/>
-        </span>
-        <span class="btn-click" :id="index+1" @click="pageChange(index + 1)" v-for="(item, index) in new Array(this.page)" :key="index">
-        {{ index + 1 }}
-        </span>
-        <span @click="nextStep" class="stepBtn">
-          <awesome icon="angle-double-right"/>
-        </span>
-      </div>
-    </div>
 
-    <!-- modal delete-->
-    <div class="modal-delete">
-      <div class="content-delete">
-         <div class="header-delete">
+      <!-- pagination -->
+      <div id="pagination">
+        <p>Show in 1 to {{ showStudents.length }} to {{ lengthArrStudents }}</p>
+        <div>
+          <span @click="previousStep">
+            <awesome icon="angle-double-left" />
+          </span>
+          <span
+            class="btn-click"
+            :id="index + 1"
+            @click="pageChange(index + 1)"
+            v-for="(item, index) in new Array(this.page)"
+            :key="index"
+          >
+            {{ index + 1 }}
+          </span>
+          <span @click="nextStep" class="stepBtn">
+            <awesome icon="angle-double-right" />
+          </span>
+        </div>
+      </div>
+
+      <!-- modal delete-->
+      <div class="modal-delete">
+        <div class="content-delete">
+          <div class="header-delete">
             <p>Bạn có muốn xóa sinh viên này!</p>
-         </div>
-         <div class="body-delete">
-           <div>
-           <span>Họ tên: </span>
-            <span class="name-delete"></span>
-           </div>
-           <div>
-           <span>Mã số sinh viên: </span>
-           <span class="maSV-delete"></span>
-           </div>
-         </div>
-         <div class="footer-delete">
-           <button class="btn btn-secondary cancelDelete radius-0px">Hủy</button>
-           <button class="btn btn-danger deleteOke radius-0px">Oke</button>
-         </div>
+          </div>
+          <div class="body-delete">
+            <div>
+              <span>Họ tên: </span>
+              <span class="name-delete"></span>
+            </div>
+            <div>
+              <span>Mã số sinh viên: </span>
+              <span class="maSV-delete"></span>
+            </div>
+          </div>
+          <div class="footer-delete">
+            <button class="btn btn-secondary cancelDelete radius-0px">
+              Hủy
+            </button>
+            <button class="btn btn-danger deleteOke radius-0px">Oke</button>
+          </div>
+        </div>
       </div>
     </div>
-  
-  </div>
   </div>
 </template>
 
@@ -97,53 +133,55 @@ export default {
       title: "http://localhost:3000/config",
       studentsJson: "http://localhost:3000/students",
       students: [],
-      page: undefined, 
+      page: undefined,
       showStudents: {
-        length:8
-      }, 
-      curentPage: 1, 
+        length: 8,
+      },
+      curentPage: 1,
       listRender: [],
       idDelete: undefined,
-     
     };
   },
-  computed:{
-    lengthArrStudents:function () {
-      return this.students.length; 
-    }
+  computed: {
+    lengthArrStudents: function () {
+      return this.students.length;
+    },
   },
   mounted() {
     this.loader();
-      document.getElementById("1").classList.add('clickPage'); 
+    document.getElementById("1").classList.add("clickPage");
   },
   watch: {
-    showStudents:{
-      deep:true,
-      handler (newShow) {
+    showStudents: {
+      deep: true,
+      handler(newShow) {
         this.curentPage = 1;
-        console.log(`${(parseInt(newShow.length, 10)+1)*50}px`);
-          const tableNode = document.getElementById('table');
-          tableNode.style.minHeight = `${(parseInt(newShow.length, 10)+1)*50}px`;
-            tableNode.style.maxHeight = `${(parseInt(newShow.length, 10)+1)*100}px`;
-            this.page = this.students.length /  newShow.length; 
-            this.listRender = this.students.slice(0,  newShow.length);
-            if (this.students.length % newShow.length != 0) {
-              this.page = Math.trunc(this.page) + 1;
-            }
-        
-     
-    }
-    } ,
+        console.log(`${(parseInt(newShow.length, 10) + 1) * 50}px`);
+        const tableNode = document.getElementById("table");
+        tableNode.style.minHeight = `${
+          (parseInt(newShow.length, 10) + 1) * 50
+        }px`;
+        tableNode.style.maxHeight = `${
+          (parseInt(newShow.length, 10) + 1) * 100
+        }px`;
+        this.page = this.students.length / newShow.length;
+        this.listRender = this.students.slice(0, newShow.length);
+        if (this.students.length % newShow.length != 0) {
+          this.page = Math.trunc(this.page) + 1;
+        }
+      },
+    },
     students() {
-      this.page = this.students.length / this.showStudents.length; 
+      this.page = this.students.length / this.showStudents.length;
       this.listRender = this.students.slice(0, this.showStudents.length);
       if (this.students.length % this.showStudents.length != 0) {
         this.page = Math.trunc(this.page) + 1;
       }
     },
-    curentPage(newVal,oldVal) {  // góa trị vừa thay đổi song 
-      document.getElementById(oldVal).classList.remove('clickPage');
-      document.getElementById(newVal).classList.add('clickPage');
+    curentPage(newVal, oldVal) {
+      // góa trị vừa thay đổi song
+      document.getElementById(oldVal).classList.remove("clickPage");
+      document.getElementById(newVal).classList.add("clickPage");
       this.listRender = this.students.slice(
         (newVal - 1) * this.showStudents.length,
         (newVal - 1) * this.showStudents.length + this.showStudents.length
@@ -152,45 +190,41 @@ export default {
   },
   methods: {
     tangshow() {
-       if (this.showStudents.length != this.students.length ) {
-      this.showStudents.length++;
+      if (this.showStudents.length != this.students.length) {
+        this.showStudents.length++;
       }
     },
     giamshow() {
-     
       if (this.showStudents.length > 8) {
         this.showStudents.length--;
       }
     },
-    confirm(id,name,ms,arrStudent) {
-      document.querySelector('.modal-delete').style.display='flex';
-      document.querySelector('.cancelDelete').addEventListener ('click', function () {
-      document.querySelector('.modal-delete').style.display='none';         
-        }
-      )
-      document.querySelector('.name-delete').innerText=name;
-      document.querySelector('.maSV-delete').innerText=ms;
-      document.querySelector('.deleteOke').addEventListener('click',
-          function () {
-            var url = 'http://localhost:3000/students/' + id;
-      fetch (url,{
-        method:'DELETE',
-         headers: {'Content-Type': 'application/json'},
-      }
-      )
-      arrStudent.forEach (
-        function (student,index,arr) {
-          if (student.id == id ) {
-            arr.splice(index,1);
-          }
-        }
-      )
-      document.querySelector('.modal-delete').style.display='none';
-          }
-        )
- 
+    confirm(id, name, ms, arrStudent) {
+      document.querySelector(".modal-delete").style.display = "flex";
+      document
+        .querySelector(".cancelDelete")
+        .addEventListener("click", function () {
+          document.querySelector(".modal-delete").style.display = "none";
+        });
+      document.querySelector(".name-delete").innerText = name;
+      document.querySelector(".maSV-delete").innerText = ms;
+      document
+        .querySelector(".deleteOke")
+        .addEventListener("click", function () {
+          var url = "http://localhost:3000/students/" + id;
+          fetch(url, {
+            method: "DELETE",
+            headers: { "Content-Type": "application/json" },
+          });
+          arrStudent.forEach(function (student, index, arr) {
+            if (student.id == id) {
+              arr.splice(index, 1);
+            }
+          });
+          document.querySelector(".modal-delete").style.display = "none";
+        });
     },
-    pageChange(numberClick) { 
+    pageChange(numberClick) {
       this.curentPage = numberClick;
       this.listRender = this.students.slice(
         (numberClick - 1) * this.showStudents.length,
@@ -227,7 +261,7 @@ export default {
           "Content-Type": "application/json",
         },
       };
-      fetch(url, options); 
+      fetch(url, options);
       this.deleteCancel();
       this.loader();
     },
@@ -241,7 +275,6 @@ export default {
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap");
 
-
 #content {
   padding: 0px 15px;
 }
@@ -253,9 +286,8 @@ export default {
   font-family: "Roboto", sans-serif;
 }
 
-
 .row-custom:nth-child(even) {
-  background-color: #E6E6FA;
+  background-color: #e6e6fa;
 }
 
 .clickPage {
@@ -263,8 +295,6 @@ export default {
   color: white;
   font-weight: 500;
 }
-
-
 
 #table {
   min-height: 470px;
@@ -283,7 +313,6 @@ export default {
   display: flex;
   justify-content: space-between;
   padding: 10px 0px;
-
 }
 
 #pagination div span {
@@ -297,12 +326,9 @@ export default {
   font-size: 16px;
 }
 
-
-
-
 .modal-delete {
   display: none;
-  position:absolute;
+  position: absolute;
   top: 0;
   right: 0;
   bottom: 0;
@@ -320,8 +346,6 @@ export default {
   height: 200px;
   flex-direction: column;
   justify-content: space-between;
-
-
 }
 
 .header-delete p {
@@ -340,11 +364,10 @@ export default {
 }
 
 .footer-delete button {
-  	width:100%;
-    height: 100%;
-    padding: 12px 0px;
-    border: none;
-    
+  width: 100%;
+  height: 100%;
+  padding: 12px 0px;
+  border: none;
 }
 
 .body-delete > div {
@@ -353,7 +376,5 @@ export default {
   opacity: 0.8;
   font-weight: 400;
 }
-
-
 </style>
 
